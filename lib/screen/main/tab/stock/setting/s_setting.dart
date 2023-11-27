0ac517499c687a565bd72b3e4dc15681dbb4c508
changjoosohn <eveckdwn@gmail.com>
2023-11-27 19:28:14 +0900

    스크롤에 따라 크기가 변하는 Title 구현하기 커스텀 Implicit Animation 구현해보기

--- lib/screen/main/tab/stock/setting/s_setting.dart
+++ lib/screen/main/tab/stock/setting/s_setting.dart
@@ -7,6 +7,7 @@ import 'package:fast_app_base/screen/main/tab/stock/setting/d_number.dart';
 import 'package:fast_app_base/screen/opensource/s_opensource.dart';
 import 'package:flutter/material.dart';
 
+import 'w_animated_app_bar.dart';
 import 'w_switch_menu.dart';
 
 class SettingScreen extends StatefulWidget {
@@ -17,72 +18,126 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  final scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: '설정'.text.make()),
-      body: ListView(
+      body: Stack(
         children: [
-          //  switch
-          Obx(
-            () => SwitchMenu(
-              '푸시 설정',
-              Prefs.isPushOnRx.get(),
-              onChanged: (isOn) {
-                Prefs.isPushOnRx.set(isOn);
-                /*
-                get package를 활용한 RxPreferenceItem를 사용하면,
-                isPushOn이 SettingScreenState의 종속적인 변수가 아니라
-                전역적으로 Prefs에 따른 한가지만 존재하는 변수로 관리하기 때문에 setState가 필요없어진다.
-                setState((){
-                  isPushOn = isOn;
-                });
-                */
-              },
-            ),
-            //  BigButton('테스트', onTap: () => Nav.push(SettingScreen())),
+          ListView(
+            padding: const EdgeInsets.only(top: 150),
+            controller: scrollController,
+            children: [
+              //  switch
+              Obx(
+                () => SwitchMenu(
+                  '푸시 설정',
+                  Prefs.isPushOnRx.get(),
+                  onChanged: (isOn) {
+                    Prefs.isPushOnRx.set(isOn);
+                    /*
+                    get package를 활용한 RxPreferenceItem를 사용하면,
+                    isPushOn이 SettingScreenState의 종속적인 변수가 아니라
+                    전역적으로 Prefs에 따른 한가지만 존재하는 변수로 관리하기 때문에 setState가 필요없어진다.
+                    setState((){
+                      isPushOn = isOn;
+                    });
+                    */
+                  },
+                ),
+                //  BigButton('테스트', onTap: () => Nav.push(SettingScreen())),
+              ),
+
+              //  slider
+              Obx(() => Slider(
+                  value: Prefs.sliderPosition.get(),
+                  onChanged: (value) {
+                    Prefs.sliderPosition.set(value);
+                  })),
+
+              //  date time
+              Obx(
+                () => BigButton(
+                    '날짜 ${Prefs.birthday.get() == null ? "" : Prefs.birthday.get()?.formattedDate}',
+                    onTap: () async {
+                  final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now().subtract(90.days),
+                      lastDate: DateTime.now().add(90.days));
+                  if (date != null) {
+                    Prefs.birthday.set(date);
+                  }
+                }),
+              ),
+
+              //  number
+              Obx(
+                () => BigButton(
+                    '저장된 숫자 ${Prefs.number.get()}',
+                    onTap: () async {
+                  final number = await NumberDialog().show();
+                  if (number != null) {
+                    Prefs.number.set(number);
+                  }
+                }),
+              ),
+
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+              BigButton(
+              '오픈소스 화면',
+              onTap: () async {
+                Nav.push(const OpensourceScreen());
+              }),
+
+            ],
           ),
-
-          //  slider
-          Obx(() => Slider(
-              value: Prefs.sliderPosition.get(),
-              onChanged: (value) {
-                Prefs.sliderPosition.set(value);
-              })),
-
-          //  date time
-          Obx(
-            () => BigButton(
-                '날짜 ${Prefs.birthday.get() == null ? "" : Prefs.birthday.get()?.formattedDate}',
-                onTap: () async {
-              final date = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now().subtract(90.days),
-                  lastDate: DateTime.now().add(90.days));
-              if (date != null) {
-                Prefs.birthday.set(date);
-              }
-            }),
-          ),
-
-          //  number
-          Obx(
-            () => BigButton(
-                '저장된 숫자 ${Prefs.number.get()}',
-                onTap: () async {
-              final number = await NumberDialog().show();
-              if (number != null) {
-                Prefs.number.set(number);
-              }
-            }),
-          ),
-
-          BigButton(
-          '오픈소스 화면',
-          onTap: () async {
-            Nav.push(const OpensourceScreen());
-          }),
+          AnimatedAppBar('설정', controller: scrollController)
         ],
       ),
     );
