c4d0f0f2fb2e020ae430cf438e4706a0cf960ec7
changjoosohn <eveckdwn@gmail.com>
2023-11-30 02:51:45 +0900

    Collections, Generic, Future, Async, Await

--- lib/screen/main/tab/home/vo/vo_bank.dart
+++ lib/screen/main/tab/home/vo/vo_bank.dart
@@ -3,4 +3,20 @@ class Bank {
   final String logoImangePath;
 
   Bank(this.name, this.logoImangePath);
+
+  @override
+  String toString() {
+    return name;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if(identical(this, other)){
+      return true;
+    }
+    if(other.runtimeType != runtimeType){
+      return false;
+    }
+    return other is Bank && other.name == name;
+  }
 }
